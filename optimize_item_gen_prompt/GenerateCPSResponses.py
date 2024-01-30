d122e7eec2f020b36b7d1c8bf7b8a43afd0bdaa4
A.V.Laverghetta@gmail.com <aml7990@i4-l-txh710-2.ad.psu.edu>
2024-01-30 11:41:11 -0500

    Fixing issue with demographics not logged int output

--- optimize_item_gen_prompt/GenerateCPSResponses.py
+++ optimize_item_gen_prompt/GenerateCPSResponses.py
@@ -44,6 +44,21 @@ class PromptGenerator:
         Solution:""",
                 ),
             ],
+            [
+                (
+                    "system",
+                    "You are a participant in an experiment. You are a {ethnicity} {gender} who works in {industry}. Your job title is {title}. You will be presented with a problem scenario, and must come up with a solution to the problem. Be creative in your response, but keep it at no more than 4 sentences in length. Respond in a single paragraph.",
+                ),
+                (
+                    "human",  # 2
+                    """Scenario:
+                    {creative_scenario}
+
+        ###
+
+        Solution:""",
+                ),
+            ],
         ]
         creative_scenario_generation_prompt = ChatPromptTemplate.from_messages(
             scenario_base_prompts[scendario_prompt_idx]
@@ -67,55 +82,127 @@ def test_creative_response(
     problem,
     prompt_idx: int,
     llm,
+    ethnicity: str = None,
+    gender: str = None,
+    industry: str = None,
+    title: str = None,
 ):
     prompt = PromptGenerator.make_creative_scenario_response_prompt(
         prompt_idx
     )  # the prompt type
 
     chain = prompt | llm
-    result = chain.invoke({"creative_scenario": problem})
+    if prompt_idx == 0:
+        result = chain.invoke({"creative_scenario": problem})
+    elif prompt_idx == 1:
+        result = chain.invoke(
+            {
+                "creative_scenario": problem,
+                "ethnicity": ethnicity,
+                "gender": gender,
+                "industry": industry,
+                "title": title,
+            }
+        )
     result = CreativityScenarioResponseParser.parse(result)
 
     return result
 
 
 def create_scenario_responses(
-    prompt_idx: int,
-    llm,
-    input_file: str = None,
+    llm, input_file: str = None, demographics_file: str = None
 ):
     input_file = pd.read_csv(
         input_file,
     )
-    ai_responses = pd.DataFrame(
-        columns=["Problem", "Dataset", "ProblemID", "set", "response"]
-    )
+    if demographics_file is not None:
+        demographics_file = pd.read_csv(demographics_file)
+    
+    if demographics_file is not None:
+        ai_responses = pd.DataFrame(
+            columns=[
+                "Problem",
+                "Dataset",
+                "ProblemID",
+                "set",
+                "response",
+                "ethnicity",
+                "gender",
+                "industry",
+                "title",
+            ]
+        )
+    else:
+        ai_responses = pd.DataFrame(
+            columns=["Problem", "Dataset", "ProblemID", "set", "response"]
+        )
     for index, row in tqdm(input_file.iterrows(), total=input_file.shape[0]):
         # generate 30 responses to each scenario
+        # we assume demographics want to be included if the file was specified
+        # if so, create a "profile" by drawing a random sample from that file
+        # do not sample them again twice
         print(row["Problem"])
         for i in tqdm(range(30)):
-            result = test_creative_response(
-                row["Problem"],
-                prompt_idx,
-                llm,
-            )
-            ai_responses = pd.concat(
-                [
-                    ai_responses,
-                    pd.DataFrame(
-                        {
-                            "Problem": row["Problem"],
-                            "response": result,
-                            "Dataset": row["Dataset"],
-                            "ProblemID": row["ProblemID"],
-                            "set": row["set"],
-                        },
-                        index=[0],
-                    ),
-                ],
-                ignore_index=True,
-            )
+            if demographics_file is not None:
+                participant = demographics_file.sample(n=1)
+                # demographics_file = demographics_file.T
+                # demographics_file.pop(participant.index)
+                # demographics_file = demographics_file.T
+                result = test_creative_response(
+                    row["Problem"],
+                    1,
+                    llm,
+                    participant["Q15"].values[0],  # ethnicity
+                    participant["Q14"].values[0],  # gender
+                    participant["Q24"].values[0],  # industry
+                    participant["Q23"].values[0],  # title
+                )
+            else:
+                result = test_creative_response(
+                    row["Problem"],
+                    0,
+                    llm,
+                )
+            if demographics_file is not None:
+                ai_responses = pd.concat(
+                    [
+                        ai_responses,
+                        pd.DataFrame(
+                            {
+                                "Problem": row["Problem"],
+                                "response": result,
+                                "Dataset": row["Dataset"],
+                                "ProblemID": row["ProblemID"],
+                                "set": row["set"],
+                                "ethnicity": participant["Q15"].values[0],
+                                "gender": participant["Q14"].values[0],
+                                "industry": participant["Q24"].values[0],
+                                "title": participant["Q23"].values[0],
+                            },
+                            index=[0],
+                        ),
+                    ],
+                    ignore_index=True,
+                )
+            else:
+                ai_responses = pd.concat(
+                    [
+                        ai_responses,
+                        pd.DataFrame(
+                            {
+                                "Problem": row["Problem"],
+                                "response": result,
+                                "Dataset": row["Dataset"],
+                                "ProblemID": row["ProblemID"],
+                                "set": row["set"],
+                            },
+                            index=[0],
+                        ),
+                    ],
+                    ignore_index=True,
+                )
 
+    # TODO: log model name and other params
     ai_responses.to_csv(
         "/home/aml7990/Code/creativity-item-generation/optimize_item_gen_prompt/ai_outputs.csv",
     )
@@ -132,6 +219,7 @@ if __name__ == "__main__":
     parser.add_argument("--prompt_idx", type=int)
     parser.add_argument("--max_tokens", type=int)
     parser.add_argument("--input_file", type=str)
+    parser.add_argument("--demographics_file", type=str, default=None)
     parser = parser.parse_args()
     model_name = parser.model_name
     temperature = parser.temperature
@@ -160,7 +248,7 @@ if __name__ == "__main__":
     llm = HuggingFacePipeline(pipeline=pipeline)
 
     create_scenario_responses(
-        parser.prompt_idx,
         llm,
         parser.input_file,
+        parser.demographics_file,
     )
