857938918b8ca4fc5256b6005f89ac4834941e9a
A.V.Laverghetta@gmail.com <aml7990@i4-l-txh710-2.ad.psu.edu>
2024-06-20 13:54:58 -0400

    First implementation of RAG using new prompt type, still rough

--- scorers/CPS/LLM/CPSManyShotLLM.py
+++ scorers/CPS/LLM/CPSManyShotLLM.py
@@ -5,10 +5,12 @@
     Optionally, explore methods for prompt optimization.
     NOTE: unlike Peft, these are all hard prompts.
 """
+
 # TODO: finish, separateme methods for tuning and inference using sklearn-like API
 import pandas as pd
 import numpy as np
 import torch
+
 # import wandb
 import evaluate
 import json
@@ -22,6 +24,8 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModelForSeq2Se
 from transformers import BitsAndBytesConfig
 from tqdm import tqdm
 from langchain.schema import StrOutputParser
+from openai import OpenAI
+
 
 # Claude
 from langchain_anthropic import ChatAnthropic
@@ -30,7 +34,7 @@ from transformers import pipeline as hf_pipeline
 from langchain.prompts.chat import ChatPromptTemplate
 from langchain.prompts import PromptTemplate
 
-from prompts import prompts
+from prompts import prompts, assistant_prompts
 from Config import few_shot_config
 from key import ANTHROPIC_KEY, OPENAI_KEY
 from random import choice
@@ -38,18 +42,25 @@ from random import choice
 RESPONSE_COL = "Solutions"
 INPUT_COL = "ProblemFull"
 
-def compute_metrics(
-    predictions, references
-):
-    predictions = [int(p) for p in predictions]
+
+def compute_metrics(predictions, references):
+    # predictions = [int(p) for p in predictions]
+    # new_preds = []
+    # for index, value in enumerate(predictions):
+    #     if type(value) == int:
+    #         new_preds.append(value)
+    #     else:
+    #         references.pop(index)
     accuracy = evaluate.load("accuracy")
-    accuracy = accuracy.compute(predictions=predictions, references=references)["accuracy"]
-    correlation = spearmanr(predictions, references)[0]
+    accuracy = accuracy.compute(predictions=predictions, references=references)[
+        "accuracy"
+    ]
+    correlation = spearmanr(predictions, references)
     return {"accuracy": accuracy, "Spearman Correlation": correlation}
 
 
 # a version of the data prep function for prompting
-def PrepareFewShotDataset(df: str) -> List:
+def PrepareFewShotDataset(df: str, item: str) -> List:
     d = pd.read_csv(df)
     d.dropna(inplace=True)
     d = d.sample(frac=1.0)
@@ -59,23 +70,25 @@ def PrepareFewShotDataset(df: str) -> List:
     middle_quartile = describe["50%"]
     upper_quartile = describe["75%"]
     d["label"] = d["FacScoresO"].apply(
-        lambda x: 0
-        if x < lower_quartile
-        else 1
-        if x < middle_quartile
-        else 2
-        if x < upper_quartile
-        else 3
+        lambda x: (
+            0
+            if x < lower_quartile
+            else 1 if x < middle_quartile else 2 if x < upper_quartile else 3
+        )
     )
 
     d["text"] = "Problem: " + d[INPUT_COL] + "\nResponse: " + d[RESPONSE_COL] + "\n"
+    problem = d[d["ProblemID"] == item].iloc[0]["ProblemFull"]
     train = d[d["set"] == "training"]
     test = d[d["set"] == "test"]
     heldout = d[d["set"] == "heldout"]
-    train = train.filter(["text", "label"], axis=1)
-    test = test.filter(["text", "label"], axis=1)
+    train = train[train["ProblemID"] == item]
+    test = test[test["ProblemID"] == item]
+    train = train.filter(["Solutions", "text", "label"], axis=1)
+    test = test.filter(["Solutions", "text", "label"], axis=1)
     heldout = heldout.filter(["text", "label"], axis=1)
-    return train, test, heldout
+    return problem, train, test, heldout
+
 
 def LLMTrial():
     # if not few_shot_config["use_sweep"]:
@@ -92,20 +105,27 @@ def LLMTrial():
     #             "PromptIdx": few_shot_config["PromptIdx"],
     #         },
     #     )
-    train_df, test_df, heldout_df = PrepareFewShotDataset(few_shot_config["dataset"])
+    problem, train_df, test_df, heldout_df = PrepareFewShotDataset(
+        few_shot_config["dataset"], few_shot_config["TrainItem"]
+    )
     # sample n exemplars to include in all prompts, use seed for reproducability
-    train_shots = train_df.groupby("label").sample(
-        n=few_shot_config["NumShots"] // len(train_df["label"].unique()),
-        replace=False,
-        random_state=few_shot_config["random_seed"],
-    )
-    train_shots["label"] = train_shots["label"].astype("str")
-    train_shots["example"] = train_shots["text"] + "Label: " + train_shots["label"]
-    # mix the order of positives and negatives
-    train_shots = train_shots.sample(
-        n=len(train_shots), random_state=few_shot_config["random_seed"], replace=False
-    )
-    train_shots = list(train_shots["example"])
+    if "asst" not in few_shot_config["ModelName"]:
+        train_shots = train_df.groupby("label").sample(
+            n=few_shot_config["NumShots"] // len(train_df["label"].unique()),
+            replace=False,
+            random_state=few_shot_config["random_seed"],
+        )
+        train_shots["label"] = train_shots["label"].astype("str")
+        train_shots["example"] = (
+            train_shots["Solutions"] + " Label: " + train_shots["label"]
+        )
+        # mix the order of positives and negatives
+        train_shots = train_shots.sample(
+            n=len(train_shots),
+            random_state=few_shot_config["random_seed"],
+            replace=False,
+        )
+        train_shots = list(train_shots["example"])
     test_df["pred_label"] = None
 
     if few_shot_config["use_test_set"]:
@@ -123,12 +143,17 @@ def LLMTrial():
     with open(few_shot_config["logFile"], "w+") as log:
         log.writelines("Starting Trial...\n")
 
-    prompt = ChatPromptTemplate.from_messages(prompts[few_shot_config["PromptIdx"]])
+    if "asst" in few_shot_config["ModelName"]:
+        prompt = ChatPromptTemplate.from_messages(
+            assistant_prompts[few_shot_config["PromptIdx"]]
+        )
+    else:
+        prompt = ChatPromptTemplate.from_messages(prompts[few_shot_config["PromptIdx"]])
 
     try:
         if few_shot_config["ModelName"] == "claude-3":
             model = ChatAnthropic(
-                model_name="claude-3-haiku-20240307", # TODO: no hard code here
+                model_name="claude-3-haiku-20240307",  # TODO: no hard code here
                 max_tokens_to_sample=few_shot_config["MaxTokens"],
                 temperature=few_shot_config["Temperature"],
                 anthropic_api_key=ANTHROPIC_KEY,
@@ -146,6 +171,16 @@ def LLMTrial():
                 max_tokens=few_shot_config["MaxTokens"],
                 model_kwargs=model_kwargs,
             )
+        elif "asst" in few_shot_config["ModelName"]:
+            # model = OpenAIAssistantRunnable(
+            #     assistant_id=few_shot_config["ModelName"],
+            #     as_agent=True,
+            # )
+            try:
+                client = OpenAI()
+                model = client.beta.assistants.retrieve(few_shot_config["ModelName"])
+            except Exception as e:
+                print("Specified assistant not found!")
         else:
             bnb_config = bnb_config = BitsAndBytesConfig(
                 bnb_16bit_compute_dtype=torch.bfloat16,
@@ -178,7 +213,7 @@ def LLMTrial():
         exit(-1)
 
     # main inference loop
-    if few_shot_config["use_test_set"]: # TODO: update
+    if few_shot_config["use_test_set"]:  # TODO: update
         for index, row in tqdm(test_df.iterrows(), total=len(test_df)):
             # invoke the llm to predict the val sample
             prompt_f = prompt.format(
@@ -207,7 +242,7 @@ def LLMTrial():
                     t5_prompt = prompt_cot
                 else:
                     t5_prompt = prompt_f
-                
+
                 input_ids = tokenizer(
                     t5_prompt, padding=True, return_tensors="pt"
                 ).input_ids.cuda()
@@ -219,7 +254,7 @@ def LLMTrial():
                     pred = int(pred)
                 except Exception:
                     pred = choice([0, 1])
-                test_df.at[index,"pred_label"] = pred
+                test_df.at[index, "pred_label"] = pred
             else:
                 prob_0 = to_tokens_and_logprobs(model, tokenizer, prompt_0)[0][-1][1]
                 prob_1 = to_tokens_and_logprobs(model, tokenizer, prompt_1)[0][-1][1]
@@ -228,15 +263,41 @@ def LLMTrial():
     else:
         for index, row in tqdm(test_df.iterrows(), total=len(test_df)):
             # invoke the llm to predict the val sample
-            completion_chain = (
-                prompt | model | StrOutputParser()
-            )
-            result = completion_chain.invoke({
-                    "examples": "\n###\n".join(train_shots),
-                    "exemplar": row["text"] + " Label: ",
-            })
-            test_df.at[index, "pred_label"] = result 
+            if "asst" in few_shot_config["ModelName"]:
+                thread = client.beta.threads.create()
+                final_prompt = prompt.format(**{
+                        "exemplar": row["Solutions"],
+                }).replace("System:","").replace("Human:","") + " Originality:"
 
+                message = client.beta.threads.messages.create(
+                    thread_id=thread.id,
+                    role="user",
+                    content=final_prompt,
+                )
+                run = client.beta.threads.runs.create_and_poll(
+                    thread_id=thread.id,
+                    assistant_id=model.id,
+                )
+                result = client.beta.threads.messages.list(
+                    thread_id=thread.id
+                ).data[0].content[0].text.value
+            else:
+                completion_chain = prompt | model | StrOutputParser()
+                result = completion_chain.invoke(
+                    {
+                        "examples": "\n###\n".join(train_shots),
+                        "exemplar": row["Solutions"] + " Label: ",
+                        "problem": problem,
+                    }
+                )
+            try:
+                result = int(result)
+            except Exception:
+                if result[-1].isdigit():
+                    result = int(result[-1])
+                else:
+                    result = choice([0, 1, 2, 3])
+            test_df.at[index, "pred_label"] = result
 
     if few_shot_config["use_test_set"]:
         preds = list(heldout_df["pred_label"])
